d027ffa811333307e38a2caa90d2db4685dc21e8
Allan <altonallan47@gmail.com>
2026-07-28 13:30:17 +0300

    chore: delete airbnb/template-component/example/convex/convex.config.ts

--- airbnb/template-component/example/convex/convex.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineApp } from "convex/server";
-import sampleComponent from "@example/sample-component/convex.config.js";
-
-const app = defineApp();
-app.use(sampleComponent, { httpPrefix: "/comments/" });
-
-export default app;
